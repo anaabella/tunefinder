ff44e671578e74946c359887d56bb38b3ecce492
Anabella Lujan Medrano <analujan761@gmail.com>
2025-11-24 12:54:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1382775899-56852",
+  "appId": "1:233250547798:web:a04e2335431455faa3f3f7",
+  "apiKey": "AIzaSyCtG4LKm8zA2rbX6ch5YShbBHF3lFNp69A",
+  "authDomain": "studio-1382775899-56852.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "233250547798"
+};
