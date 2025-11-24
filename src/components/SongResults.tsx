3a1580911e7b5d3945aea86d6594e346a2c435b1
Anabella Lujan Medrano <analujan761@gmail.com>
2025-11-24 16:56:24 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/components/SongResults.tsx
+++ src/components/SongResults.tsx
@@ -11,6 +11,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
 import { useToast } from '@/hooks/use-toast';
